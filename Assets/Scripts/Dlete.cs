d79f6de3070cfb95dcde95b490c699d255e376ca
KATKAMSAIPREM <139459646+katkamsaiprem@users.noreply.github.com>
2023-11-11 00:15:20 +0530

    awd

--- Assets/Scripts/Dlete.cs
+++ Assets/Scripts/Dlete.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class temp2 : MonoBehaviour
+public class Dlete : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
